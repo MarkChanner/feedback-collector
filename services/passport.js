6af151cf4467ffcddfdb0ed29503ef799ef75a4b
Mark Channer <markchanner@gmail.com>
2019-12-06 18:29:28 +0000

    First commit

---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+// the user is from done(null, user) in User.findOne
+passport.serializeUser((user, done) => {
+  done(null, user.id); // user.id is shortcut to _id of the user in MongoDB
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);
